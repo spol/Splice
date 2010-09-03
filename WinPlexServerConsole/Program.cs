faf12f5583d140098ed8796e8cf5a76048fb3b0e
Seb Pollard <seb.pollard@spookmedia.com>
2010-09-03 15:55:47 +0100

    Initial commit.

---
+++ WinPlexServerConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Text;
+using WinPlexServer;
+
+namespace WinPlexServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ServerTestUrl = "http://localhost:32400/";
+            using (WebServer listener = new WebServer(new Uri(ServerTestUrl), new Router()))
+            {
+                //listener.IncomingRequest += WebServer_IncomingRequest;
+                listener.Start();
+                Console.WriteLine("Listener accepting requests: {0}", listener.RunState == WebServer.State.Started);
+                //Console.WriteLine("Making requests...");
+                //while (true)
+                //{
+                //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ServerTestUrl);
+                //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                //    using (Stream responseStream = response.GetResponseStream())
+                //    using (StreamReader responseStreamReader = new StreamReader(responseStream, Encoding.UTF8))
+                //    {
+                //        Console.WriteLine(responseStreamReader.ReadToEnd());
+                //    }
+                //    System.Threading.Thread.Sleep(1000);
+                //}
+
+                Console.WriteLine("Done. Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WinPlexServerLib/Router.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace WinPlexServer
+{
+    public class Router
+    {
+        public void IncomingRequest(object sender, HttpRequestEventArgs e)
+        {
+            //e.RequestContext.Request.Url;
+            //e.RequestContext.Request.QueryString;
+            HttpListenerResponse response = e.RequestContext.Response;
+            string content = e.RequestContext.Request.Url.AbsolutePath;
+            byte[] buffer = Encoding.UTF8.GetBytes(content);
+            response.StatusCode = (int)HttpStatusCode.OK;
+            response.StatusDescription = "OK";
+            response.ContentLength64 = buffer.Length;
+            response.ContentEncoding = Encoding.UTF8;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+            response.Close();
+        }
+    }
+}
