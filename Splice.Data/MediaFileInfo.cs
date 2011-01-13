204be6542beee725e9b3391cbed15a0746e5cd50
Seb Pollard <seb@spookmedia.com>
2011-01-13 14:07:47 +0000

    Forgot to add new Media/VideoFileInfo classes.

---
+++ Splice.Data/MediaFileInfo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Splice.Data
+{
+    public abstract class MediaFileInfo
+    {
+        private string _Filename;
+
+        public Int32 Id { get; set; }
+        public string Filename { get { return System.IO.Path.GetFileName(Path); } }
+        public Int32 Size { get; set; }
+        public string Path { get { return _Filename; } set { _Filename = value; } }
+        public Int32 Duration { get; set; }
+
+        public bool Exists
+        {
+            get
+            {
+                return File.Exists(Path);
+            }
+        }
+
+        public string GetRelativePath(string Root)
+        {
+            if (Path.StartsWith(Root))
+            {
+                return Path.Remove(0, Root.Length);
+            }
+            else
+            {
+                return Path;
+            }
+        }
+
+        public MediaFileInfo(string Filename)
+        {
+            _Filename = Filename;
+        }
+
+        public MediaFileInfo(DataRow Row)
+        {
+            Path = Row["path"].ToString();
+            Id = Convert.ToInt32(Row["id"]);
+            Size = Convert.ToInt32(Row["size"]);
+            Duration = 0;
+        }
+
+        public string Hash
+        {
+            get
+            {
+                if (!Exists)
+                {
+                    throw new FileNotFoundException("File doesn't exists.", Path);
+                }
+
+                FileStream File = new FileStream(Path, FileMode.Open);
+
+                byte[] Bytes;
+                if (File.Length < 200)
+                {
+                    Bytes = new byte[File.Length];
+                    File.Read(Bytes, 0, Bytes.Length);
+                }
+                else
+                {
+                    byte[] StartBuffer = new byte[100];
+                    byte[] EndBuffer = new byte[100];
+                    File.Read(StartBuffer, 0, StartBuffer.Length);
+                    File.Seek(File.Length - EndBuffer.Length, SeekOrigin.Begin);
+
+                    Bytes = new byte[StartBuffer.Length + EndBuffer.Length];
+                    System.Buffer.BlockCopy(StartBuffer, 0, Bytes, 0, StartBuffer.Length);
+                    System.Buffer.BlockCopy(EndBuffer, 0, Bytes, StartBuffer.Length, EndBuffer.Length);
+                }
+
+                System.Security.Cryptography.MD5CryptoServiceProvider cryptHandler;
+                cryptHandler = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] Hash = cryptHandler.ComputeHash(Bytes);
+
+                string HashString = "";
+                foreach (byte B in Hash)
+                {
+                    if (B < 16)
+                        HashString += "0" + B.ToString("x");
+                    else
+                        HashString += B.ToString("x");
+                }
+
+                File.Close();
+
+                return HashString;
+            }
+        }
+    }
+}
---
+++ Splice.Data/VideoFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Splice.Data
+{
+    public class VideoFileInfo : MediaFileInfo
+    {
+        public Int32 Bitrate { get; set; }
+        public Double AspectRatio { get; set; }
+        public Double AudioChannels { get; set; }
+        public string AudioCodec { get; set; }
+        public string VideoCodec { get; set; }
+        public string VideoResolution { get; set; }
+        public Double VideoFrameRate { get; set; }
+
+        public VideoFileInfo(string Filename) : base(Filename) { }
+
+        public VideoFileInfo(DataRow Row) : base(Row)
+        {
+            Bitrate = Convert.ToInt32(Row["bitrate"]);
+            AspectRatio = Convert.ToDouble(Row["aspectRatio"]);
+            AudioChannels = Convert.ToDouble(Row["audioChannels"]);
+            AudioCodec = Row["audioCodec"].ToString();
+            VideoCodec = Row["videoCodec"].ToString();
+            VideoFrameRate = Row["videoFrameRate"] == DBNull.Value ? 0 : Convert.ToDouble(Row["videoFrameRate"]);
+            VideoResolution = Row["videoResolution"].ToString();
+        }
+    }
+}
