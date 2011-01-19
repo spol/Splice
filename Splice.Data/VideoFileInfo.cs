a2acc4faf08791d8a9a504d75fde7e9412ff4696
Spol <seb@spolster.co.uk>
2011-01-19 22:43:38 +0000

    Created Splice.MediaInfo Project, and added metadata loading to the VideoFileInfo class.

--- Splice.Data/VideoFileInfo.cs
+++ Splice.Data/VideoFileInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using Splice.MediaInfoLib;
 
 namespace Splice.Data
 {
@@ -11,10 +12,11 @@ namespace Splice.Data
         public Int32 Bitrate { get; set; }
         public Double AspectRatio { get; set; }
         public Double AudioChannels { get; set; }
-        public string AudioCodec { get; set; }
-        public string VideoCodec { get; set; }
-        public string VideoResolution { get; set; }
-        public Double VideoFrameRate { get; set; }
+        public String AudioCodec { get; set; }
+        public String VideoCodec { get; set; }
+        public Int32 PictureHeight { get; set; }
+        public Int32 PictureWidth { get; set; }
+        public Double FrameRate { get; set; }
 
         public VideoFileInfo(string Filename) : base(Filename) { }
 
@@ -25,8 +27,29 @@ namespace Splice.Data
             AudioChannels = Convert.ToDouble(Row["audioChannels"]);
             AudioCodec = Row["audioCodec"].ToString();
             VideoCodec = Row["videoCodec"].ToString();
-            VideoFrameRate = Row["videoFrameRate"] == DBNull.Value ? 0 : Convert.ToDouble(Row["videoFrameRate"]);
-            VideoResolution = Row["videoResolution"].ToString();
+            FrameRate = Row["videoFrameRate"] == DBNull.Value ? 0 : Convert.ToDouble(Row["videoFrameRate"]);
+            PictureWidth = Convert.ToInt32(Row["PictureWidth"]);
+            PictureHeight = Convert.ToInt32(Row["PictureHeight"]);
+        }
+
+        public void LoadMetaDataFromFile()
+        {
+            MediaInfo Info = new MediaInfo();
+            Info.Open(Path);
+            //String Params = Info.Option("Info_Parameters");
+            Bitrate = Convert.ToInt32(Info.Get(StreamKind.General, 0, "BitRate"));
+            Duration = Convert.ToInt32(Info.Get(StreamKind.General, 0, "Duration"));
+
+            VideoCodec = Info.Get(StreamKind.Video, 0, "Format");
+            PictureWidth = Convert.ToInt32(Info.Get(StreamKind.Video, 0, "Width"));
+            PictureHeight = Convert.ToInt32(Info.Get(StreamKind.Video, 0, "Height"));
+            FrameRate = Convert.ToDouble(Info.Get(StreamKind.Video, 0, "FrameRate"));
+            AspectRatio = Convert.ToDouble(Info.Get(StreamKind.Video, 0, "DisplayAspectRatio"));
+
+            AudioCodec = Info.Get(StreamKind.Audio, 0, "Format");
+            AudioChannels = Convert.ToDouble(Info.Get(StreamKind.Audio, 0, "Channel(s)"));
+
+            Info.Close();
         }
     }
 }
