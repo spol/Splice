efd61a799936f854641fd8c30e5dca468f875d85
spol <seb@spolster.co.uk>
2011-01-31 16:30:59 +0000

    Initial loading of collections into the Manager GUI.

---
+++ Splice.Manager/Main.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinPlexManager
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CollectionsListPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.DebugOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CollectionsListPanel
+            // 
+            this.CollectionsListPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.CollectionsListPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.CollectionsListPanel.Location = new System.Drawing.Point(0, 0);
+            this.CollectionsListPanel.Name = "CollectionsListPanel";
+            this.CollectionsListPanel.Size = new System.Drawing.Size(200, 454);
+            this.CollectionsListPanel.TabIndex = 0;
+            // 
+            // DebugOutput
+            // 
+            this.DebugOutput.Location = new System.Drawing.Point(418, 110);
+            this.DebugOutput.Multiline = true;
+            this.DebugOutput.Name = "DebugOutput";
+            this.DebugOutput.Size = new System.Drawing.Size(461, 288);
+            this.DebugOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 454);
+            this.Controls.Add(this.DebugOutput);
+            this.Controls.Add(this.CollectionsListPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel CollectionsListPanel;
+        private System.Windows.Forms.TextBox DebugOutput;
+    }
+}
+
---
+++ Splice.Manager/Main.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using Splice.Data;
+
+namespace WinPlexManager
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            List<VideoCollection> Collections = GetCollectionsList();
+
+            foreach (VideoCollection Collection in Collections)
+            {
+                Label CollectionLabel = new Label();
+                CollectionLabel.Text = Collection.Title;
+                CollectionsListPanel.Controls.Add(CollectionLabel);
+            }
+
+
+        }
+
+        private List<VideoCollection> GetCollectionsList()
+        {
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("http://localhost:32400/manage/collections/list");
+            HttpWebResponse Response = (HttpWebResponse)Request.GetResponse();
+
+            XmlTextReader XmlReader = new XmlTextReader(Response.GetResponseStream());
+            
+            while (XmlReader.NodeType != XmlNodeType.Element && XmlReader.Name != "Collections" && !XmlReader.EOF)
+            {
+                XmlReader.Read();
+            }
+
+            List<VideoCollection> Collections = new List<VideoCollection>();
+            VideoCollection Collection = null;
+
+            if (!XmlReader.EOF && XmlReader.Name == "Collections")
+            {
+                while (!XmlReader.EOF)
+                {
+                    //DebugOutput.Text += XmlReader.Name;
+                    XmlReader.Read();
+                    if (XmlReader.Name == "Collection" && XmlReader.NodeType == XmlNodeType.Element)
+                    {
+                        Collection = new VideoCollection();
+                        Collection.Title = XmlReader["title"];
+                        Collection.Id = Convert.ToInt32(XmlReader["collectionId"]);
+                        Collection.Art = XmlReader["art"];
+                        Collection.Type = (VideoCollectionType)Enum.Parse(typeof(VideoCollectionType), XmlReader["type"]);
+
+                        XmlReader.Read();
+                        while (XmlReader.NodeType != XmlNodeType.EndElement && XmlReader.Name != "Collection" && XmlReader.EOF)
+                        {
+                            if (XmlReader.Name == "Location")
+                            {
+                                Collection.Locations.Add(XmlReader["path"]);
+                            }
+                            XmlReader.Read();
+                        }
+                        Collections.Add(Collection);
+                        Collection = null;
+                    }
+
+                    if (XmlReader.Name == "Collections" && XmlReader.NodeType == XmlNodeType.EndElement)
+                    {
+                        break;
+                    }
+                }
+
+                return Collections;
+            }
+            
+            throw new Exception("Collections element not found.");
+            
+        }
+    }
+}
